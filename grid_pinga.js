ec80cec0adf468afc96d1cb6639f64024ee32af9
Caley Jack <caleyjack@me.com>
2019-09-13 10:40:54 -0400

    Stagger the pings

--- grid_pinga.js
+++ grid_pinga.js
@@ -18,13 +18,31 @@ function coordsToNumber(array, posY, posX) {
   let counter = 0;
   // Count the number of items in each row preceeding posY
   for (let row = 0; row < posY; row++) {
-    // console.log(array[row].length);
     for (let x = 0; x < array[row].length; x++) {
       counter++;
     }
   }
   // Return the count of items in preceeding rows plus the X position
   return counter + posX;
+}
+
+/**
+ * Returns a set of coordinates from a number based on the width and height of the grid.
+ * @param {Integer} num - A representation of a position on the grid, starts at 0. This means an 8x8 grid with 64 buttons has a starting value of 0 and a maximum value of 63
+ * @param {Integer} sizeY - The height of the grid (rows)
+ * @param {Integer} sizeX - The width of the grid (columns)
+ */
+function numToCoords(num, sizeY, sizeX) {
+  if (num < sizeX) return { y: 0, x: num };
+
+  const offsetNum = num;
+  const quotientY = Math.floor(offsetNum / sizeX);
+  const remainderX = offsetNum % sizeX;
+
+  if (quotientY >= sizeY)
+    return new Error("The number provided falls outside the grid");
+
+  return { y: quotientY, x: remainderX };
 }
 
 async function run() {
@@ -37,28 +55,34 @@ async function run() {
 
   let grid = await monomeGrid();
   let led = create2DArray(sizeY, sizeX);
+  let step = 0;
+  let frequency = 100; // in milliseconds
 
   let refresh = function() {
-    for (let y = 0; y < sizeY; y++) {
-      for (let x = 0; x < sizeX; x++) {
-        const ledNum = coordsToNumber(led, y, x);
+    if (step >= hosts.length) step = 0;
+    const { y, x } = numToCoords(step, sizeY, sizeX);
 
-        if (ledNum < hosts.length) {
-          led[y][x] = brightness;
-        }
+    // turn on the current step
+    led[y][x] = brightness;
 
-        ping.promise.probe(hosts[ledNum]).then(res => {
-          if (res.alive) led[y][x] = 0;
-          grid.refresh(led);
-        });
+    ping.promise.probe(hosts[step]).then(res => {
+      if (res.alive) {
+        // Most times the repsonse is too fast for us to see the light
+        // turn on and off so we set a short delay before turning it off.
+        setTimeout(() => {
+          led[y][x] = 0;
+        }, 100);
       }
-    }
+    });
+
+    grid.refresh(led);
+    step++;
   };
 
   // Refresh the grid
-  setInterval(refresh, 2000);
+  setInterval(refresh, frequency);
 
-  // set up key handler
+  // Set up key handler
   grid.key(async (x, y, s) => {
     const position = coordsToNumber(led, y, x);
     const host = hosts[position];
